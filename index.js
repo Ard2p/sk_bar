e81c466dee8f06bd45b5da56d20d992798d4f182
Ard2p <ard2p@bk.ru>
2023-03-13 16:55:21 +0300

    правка структуры

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ DB.sequelize.sync().then(() => {
   console.log('Synced db.')
   bot.launch()
   // io.listen(4747)
-  let webPort = 4747
-  web.listen(webPort, () => {
-    console.log(`Up and Running on port ${webPort}- This is Customer service`);
-  })
+  // let webPort = 4747
+  // web.listen(webPort, () => {
+  //   console.log(`Up and Running on port ${webPort}- This is Customer service`);
+  // })
 }).catch((err) => {
   console.log('Failed to sync db: ' + err.message)
 })
--- web.js
+++ web.js
@@ -1,7 +1,15 @@
+const fs = require('fs')
+const http = require('http')
+const https = require('https')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+
 const bot = require('./bot')
+
+const privateKey = fs.readFileSync('./sk.key', 'utf8')
+const certificate = fs.readFileSync('./sk.crt', 'utf8')
+const credentials = { key: privateKey, cert: certificate }
 
 app.use(express.json())
 app.use(cors())
@@ -11,7 +19,7 @@ app.post('/order', (req, res) => {
 
     let text = ''
     Object.values(req.body.products).forEach((product) => {
-        text+= product.name + ': ' + product.quantity + '\n'
+        text += product.name + ': ' + product.quantity + '\n'
     })
 
     bot.telegram.sendMessage('-1001621383069',
@@ -20,5 +28,12 @@ app.post('/order', (req, res) => {
     res.send({ success: true })
 
 })
+
+
+var httpServer = http.createServer(app)
+var httpsServer = https.createServer(credentials, app)
+
+httpServer.listen(4748)
+httpsServer.listen(4747)
 
 module.exports = app
